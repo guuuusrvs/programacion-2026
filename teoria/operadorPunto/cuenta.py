187840cf82d3dcd6832c4c304c603b23d1c8c8dc
guuuusrvs <gustavorvs_14@ciencias.unam.mx>
2026-04-29 09:08:52 -0600

    Add Cuenta class with initial attributes

---
+++ teoria/operadorPunto/cuenta.py
@@ -0,0 +1,9 @@
+class Cuenta:
+    #crea una cuenta con datos iniciales
+    def __init__(self, saldo, tipo, titular):
+        
+        self.saldo = saldo
+
+        self.tipo = tipo
+
+        self.titular = titular
