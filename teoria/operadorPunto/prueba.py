05cfdb40d12118661d50acae48ab5f8ac0b9438b
guuuusrvs <gustavorvs_14@ciencias.unam.mx>
2026-04-29 09:11:29 -0600

    Create prueba.py for testing object attributes
    Add prueba.py to demonstrate object attribute access

---
+++ teoria/operadorPunto/prueba.py
@@ -0,0 +1,9 @@
+cuenta1 = Cuenta(5000, "Ahorro", "Gustavo")
+
+# el operador punto (.) se usa para acceder a los atributos del objeto
+
+print(cuenta1.saldo)
+print(cuenta1.tipo) 
+print(cuenta1.titular)
+cuenta1.saldo = 7500
+print(cuenta1.saldo)
